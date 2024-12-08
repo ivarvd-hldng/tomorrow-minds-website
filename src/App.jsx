aba2d2042908bcfb0c2e1470229175550c770eb0
ivarvd-hldng <ivarvd@gmail.com>
2024-12-08 14:53:01 +0100

    fix: Add default export to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,1 @@
-import Website from './components/Website'
-
-function App() {
-  return <Website />
-}
-
-export default App
+aW1wb3J0IFdlYnNpdGUgZnJvbSAnLi9jb21wb25lbnRzL1dlYnNpdGUnCgpmdW5jdGlvbiBBcHAoKSB7CiAgcmV0dXJuIDxXZWJzaXRlIC8+Cn0KCmV4cG9ydCBkZWZhdWx0IEFwcA==
