5ca960afdbf6ba6dd328fd89859e8315ec74a463
ivarvd-hldng <ivarvd@gmail.com>
2024-12-09 21:48:04 +0100

    Update App.jsx to include Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 import AppRoutes from './routes';
 
 const App = () => (
   <Router>
-    <div className="min-h-screen">
+    <div className="min-h-screen flex flex-col">
       <Navigation />
-      <main className="pt-16">
+      <main className="flex-grow pt-16">
         <AppRoutes />
       </main>
+      <Footer />
     </div>
   </Router>
 );
