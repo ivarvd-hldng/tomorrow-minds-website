a12998a9a14ac9843901c8a9851b001d966d09ae
ivarvd-hldng <ivarvd@gmail.com>
2024-12-10 08:38:33 +0100

    Add shadcn/ui configuration files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
