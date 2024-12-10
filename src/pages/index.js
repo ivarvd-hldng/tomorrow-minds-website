bfebd01da6f8d33deed2e86708e292183cdc214d
ivarvd-hldng <ivarvd@gmail.com>
2024-12-10 08:43:21 +0100

    Fix page imports with correct file extensions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-export { default as ServicesPage } from './ServicesPage';
-export { default as AboutPage } from './AboutPage';
-export { default as BlogPage } from './BlogPage';
-export { default as CareersPage } from './CareersPage';
-export { default as PrivacyPolicy } from './legal/PrivacyPolicy';
-export { default as TermsAndConditions } from './legal/TermsAndConditions';
+export { default as ServicesPage } from './ServicesPage.jsx';
+export { default as AboutPage } from './AboutPage.jsx';
+export { default as BlogPage } from './BlogPage.jsx';
+export { default as CareersPage } from './CareersPage.jsx';
+export { default as PrivacyPolicy } from './legal/PrivacyPolicy.jsx';
+export { default as TermsAndConditions } from './legal/TermsAndConditions.jsx';
