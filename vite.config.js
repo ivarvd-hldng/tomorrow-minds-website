c2c651a039bfb67e345f75a19ce38864fd91aba6
ivarvd-hldng <ivarvd@gmail.com>
2024-12-10 08:46:55 +0100

    Update Vite config with additional path aliases

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@app': path.resolve(__dirname, './src/app')
     },
   },
 })
